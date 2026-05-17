e3619b1586c61b997517a01a0669d044835eca04
tawbu <tawbury@gmail.com>
2026-05-17 16:26:27 +0900

    chore: ignore local mcp-cli workspace

--- mcp-cli/src/commands/index.ts
+++
@@ -1,3 +0,0 @@
-export { initCommand } from './init';
-export { installCommand } from './install';
-export { syncCommand } from './sync';
--- mcp-cli/src/src/index.ts
+++
@@ -1,6 +0,0 @@
-export { initCommand, InitArgs } from './commands/init';
-export { installCommand, InstallArgs } from './commands/install';
-export { syncCommand, SyncArgs } from './commands/sync';
-
-export * from './utils/file';
-export * from './utils/yaml';
